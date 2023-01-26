f26d8fce5f98db713ff36ac304dfbf058851e99a
Allison Lowe <alliisonlowe@gmail.com>
2023-01-26 15:59:54 -0800

    Creating client and server folders for full stack project

---
+++ client/src/components/ListWeather.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ListWeather = () =>{
+    return(
+        <h1>List</h1>
+    )
+};
+
+export default ListWeather;
---
+++ client/src/components/SearchWeather.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SearchWeather = () =>{
+    return(
+        <h1>List</h1>
+    )
+};
+
+export default SearchWeather;
