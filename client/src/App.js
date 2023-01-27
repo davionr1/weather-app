b7a180dc7dca563578ee843c74730191c0e6e9ff
Davion Roachford <davionr1@icloud.com>
2023-01-26 19:25:48 -0800

    added api to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+
+
+const api_key=process.env.REACT_APP_WEATHER_API_KEY;
+// const api_key ='HSYWPZH9LXRTYUNAC5HYXZLX6'
+const weather_url = (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/losangeles?unitGroup=us&key=${api_key}&contentType=json`);
 
 function App() {
+  
+  
+
+  
+//fetch data and output in console
+  
+    
+    fetch(weather_url)
+    .then(response => response.json())
+    .then(resData => console.log(resData))
+   
+   
   return (
     <div className="App">
-      
+       
     </div>
   );
 }
 
 export default App;
+
