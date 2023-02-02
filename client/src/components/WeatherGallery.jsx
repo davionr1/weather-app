aa16ed5f162cce559339994b4140c15ad3762e7a
Davion Roachford <davionr1@icloud.com>
2023-02-02 07:04:00 -0800

    fixed weatheritem, now shows cards

--- client/src/components/WeatherGallery.jsx
+++ client/src/components/WeatherGallery.jsx
@@ -7,30 +7,6 @@ function weatherGallery(props) {
     })
 
     console.log('city', props.data)
-    return (
-         <div className="section section_temperature">
-              <div className="icon">
-                <h3>Los Angeles, CA</h3>
-                <img
-                  src="https://cdn-icons-png.flaticon.com/128/414/414927.png"
-                  alt="Weather icon"
-                />
-                <h3>Cloudy</h3>
-              </div>
-              <div className="temperature">
-                <h1>77 °F</h1>
-              </div>
-            </div>
-         
-
-         //only a test for styling
-        /* <p className='city'>los angeles</p>
-         <p className='temperature'>77°</p>
-         <p className='weather-description'>cloudy</p>
-         <p className='max-temp'>88°</p>
-         <p className='min-temp'></p> */
-
-    )
 };
 
 export default weatherGallery;
--- client/src/components/WeatherItem.jsx
+++ client/src/components/WeatherItem.jsx
@@ -1,41 +1,67 @@
-import { FaArrowDown } from "react-icons/fa";
+import { FaArrowDown, FaArrowUp, FaWind } from "react-icons/fa";
+import { WiHumidity } from "react-icons/wi";
 import './weather.css'
 import React from "react";
 const WeatherItem = (props) => {
     return (
-        <div className='section section_descriptions'>
-            <div className='head'>
-            {/* input loc, temp, min max temp, description */}
+        <div
+      className="App"
+      style={{
+        backgroundImage:
+          "url('https://rare-gallery.com/uploads/posts/124416-miui-8-rainy-weather-background-minimal-hd.png')",
+      }}
+    >
+    
+      
 
+      
+      {/* Input Weather items in app js for now for styling */}
+      <div className="section section_temperature">
+              <div className="icon">
+                <h3>Los Angeles, CA</h3>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/414/414927.png"
+                  alt="Weather icon"
+                />
+                <h3>Cloudy</h3>
+              </div>
+              <div className="temperature">
+                <h1>77 °F</h1>
+              </div>
             </div>
-            <div className='card'>
-                <div className='description_card-icon'>
-                    <FaArrowDown />
-                    <small>air quality</small>
-                </div>
-                <h2>25 °F</h2>
-            </div>
-            <div className='card'>
-                <div className='description_card-icon'>
-                    <FaArrowDown />
-                    <small>min</small>
-                </div>
-                <h2>{props.item.minTemp}</h2>
-            </div>
-            <div className='card'>
-                <div className='description_card-icon'>
-                    <FaArrowDown />
-                    <small>max</small>
-                </div>
-                <h2>25 °F</h2>
-            </div>
-            <div className='card'>
-                <div className='description_card-icon'>
-                    <FaArrowDown />
-                    <small>humidity</small>
-                </div>
-                <h2>25 °F</h2>
-            </div>
+      <div className="section section_descriptions">
+        <div className="card">
+          <div className="description_card-icon">
+            <FaArrowDown />
+            <small>min</small>
+          </div>
+          <h2>65 °F</h2>
+        </div>
+        <div className="card">
+          <div className="description_card-icon">
+            <FaArrowUp />
+            <small>max</small>
+          </div>
+          <h2>78 °F</h2>
+        </div>
+        <div className="card">
+          <div className="description_card-icon">
+            <FaWind />
+            <small>wind speed</small>
+          </div>
+          <h2>2mph</h2>
+        </div>
+        <div className="card">
+          <div className="description_card-icon">
+            <WiHumidity />
+            <small>humidity</small>
+          </div>
+          <h2>41%</h2>
+        </div>
+      </div>
+    </div>
+    )
+}
             {/* code to output data but not working for now  */}
             {/* after data is able to be outputted, plug props data into the styling  */}
             {/*  <p className='city'>{props.item.city}</p>
@@ -49,10 +75,5 @@ const WeatherItem = (props) => {
                 <p className='daily-card'>{props.item.days}</p>
                 <p className='air-quality'></p>
                 <p className='precipitation'></p>*/}
-        </div>
-    )
-}
-
-
 export default WeatherItem;
 
